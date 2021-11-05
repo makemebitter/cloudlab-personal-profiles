f27a049532f25c3d47d40602591901c5e6200a8c
Yuhao Zhang <yhzhang1994@gmail.com>
2021-11-05 11:01:17 -0700

    loggers

--- logs/bin/run_loggers.sh
+++ logs/bin/run_loggers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NFS_DIR=${1:-"/mnt/nfs"}
-GPU_WORKERS=$2
-duty=$3
+# GPU_WORKERS=$2
+# duty=$3
 datestr=`date "+%Y_%m_%d"`
 NETWORK_LOG_DIR=$NFS_DIR/logs/${datestr}/network_logs
 DISK_LOG_DIR=$NFS_DIR/logs/${datestr}/disk_logs
@@ -12,6 +12,6 @@ nohup bash ${__dir}/network_logger.sh $NETWORK_LOG_DIR eno1 enp94s0f0 &
 nohup bash ${__dir}/disk_logger.sh $DISK_LOG_DIR &
 nohup bash ${__dir}/cpu_logger.sh $CPU_LOG_DIR &
 
-if [ $GPU_WORKERS -eq 1 ]; then
+if [ $GPU_ENABLED -eq 1 ]; then
 nohup bash ${__dir}/gpu_logger.sh $GPU_LOG_DIR &
 fi
