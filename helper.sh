58343eb2cda27cb1bc52bedd4fbf96940ffeed51
Yuhao Zhang <yhzhang1994@gmail.com>
2022-04-13 12:39:42 -0700

    update

--- helper.sh
+++ helper.sh
@@ -266,7 +266,7 @@ generally_good_stuff (){
 
 }
 
-install_cuda (){
+install_cuda_1804 (){
     # Add NVIDIA package repositories
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
     sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -293,6 +293,40 @@ install_cuda (){
     space_saver "/usr/local/cuda-11.0" "usr.local.cuda"
 
 }
+
+install_cuda (){
+    # Add NVIDIA package repositories
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+    sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+    sudo apt-get update
+
+    wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+
+    sudo apt install ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+    sudo apt-get update
+
+
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/libnvinfer8_8.0.0-1+cuda11.0_amd64.deb
+    sudo apt-get install -y ./libnvinfer8_8.0.0-1+cuda11.0_amd64.deb
+    sudo apt-get update
+
+    # Install development and runtime libraries (~4GB)
+    sudo apt-get install -y --no-install-recommends --allow-downgrades \
+        cuda-11-0 \
+        libcudnn8=8.0.5.39-1+cuda11.0  \
+        libcudnn8-dev=8.0.5.39-1+cuda11.0
+
+    sudo apt-get install -y --no-install-recommends libnvinfer8=8.0.0-1+cuda11.0 \
+        libnvinfer-dev=8.0.0-1+cuda11.0 \
+        libnvinfer-plugin8=8.0.0-1+cuda11.0
+
+    # Save space
+    space_saver "/usr/local/cuda-11.0" "usr.local.cuda"
+
+}
+
 
 install_python_dep (){
     echo No
